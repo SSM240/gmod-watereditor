1cbb325d2afaa87dd654a6cd65496e1da386f856
SSM24 <supersupermario077@gmail.com>
2022-08-24 03:03:27 -0400

    change sprite in lönn

--- Loenn/entities/redirectableBooster.lua
+++ Loenn/entities/redirectableBooster.lua
@@ -2,7 +2,7 @@ local redirectableBooster = {}
 
 redirectableBooster.name = "SSMHelper/RedirectableBooster"
 redirectableBooster.depth = -8500
-redirectableBooster.texture = "objects/booster/boosterRed00"
+redirectableBooster.texture = "objects/SSMHelper/boosters/pink/boosterPink00"
 redirectableBooster.placements = {
     name = "normal"
 }
