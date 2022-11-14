8d3ed2a2a879a128cf7a57d2314d4d2c5c606fcb
SSM240 <supersupermario077@gmail.com>
2022-11-14 02:43:36 -0500

    change selection rect for redirectable booster

--- Loenn/entities/redirectableBooster.lua
+++ Loenn/entities/redirectableBooster.lua
@@ -1,3 +1,5 @@
+local utils = require("utils")
+
 local redirectableBooster = {}
 
 redirectableBooster.name = "SSMHelper/RedirectableBooster"
@@ -6,5 +8,10 @@ redirectableBooster.texture = "objects/SSMHelper/boosters/pink/boosterPink00"
 redirectableBooster.placements = {
     name = "normal"
 }
+
+-- padding on the texture makes the default selection larger than it should be
+function redirectableBooster.selection(room, entity)
+    return utils.rectangle(entity.x - 9, entity.y - 9, 18, 18)
+end
 
 return redirectableBooster
