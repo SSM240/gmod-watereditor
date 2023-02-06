adb96d4ef1cebb7fcc98a55bd2cac7c1aa22f015
SSM240 <supersupermario077@gmail.com>
2023-02-05 19:10:12 -0500

    add Force Cassette Block Controller

---
+++ Loenn/entities/forceCassetteBlockController.lua
@@ -0,0 +1,10 @@
+local forceCassetteBlockController = {}
+
+forceCassetteBlockController.name = "SSMHelper/ForceCassetteBlockController"
+forceCassetteBlockController.depth = 0
+forceCassetteBlockController.texture = "loenn/SSMHelper/forcecassetteblockcontroller"
+forceCassetteBlockController.placements = {
+    name = "normal"
+}
+
+return forceCassetteBlockController
