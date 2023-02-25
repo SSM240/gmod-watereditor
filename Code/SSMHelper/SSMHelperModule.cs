8d204114aeeb688e6fa0aba82ebf0069b9f0000a
SSM240 <supersupermario077@gmail.com>
2023-02-25 18:35:00 -0500

    port Resizable Dash Switch from SJ

--- Code/SSMHelper/SSMHelperModule.cs
+++ Code/SSMHelper/SSMHelperModule.cs
@@ -10,6 +10,9 @@ namespace Celeste.Mod.SSMHelper
     {
         public static SSMHelperModule Instance;
 
+        public static SpriteBank SpriteBank => Instance._CustomEntitySpriteBank;
+        private SpriteBank _CustomEntitySpriteBank;
+
         public SSMHelperModule()
         {
             Instance = this;
@@ -19,6 +22,7 @@ namespace Celeste.Mod.SSMHelper
         {
             StarjumpTilesetHelper.Load();
             ReverseKillbox.Load();
+            ResizableDashSwitch.Load();
             //ChangeMaxCassetteTrigger.Load();
         }
 
@@ -26,7 +30,17 @@ namespace Celeste.Mod.SSMHelper
         {
             StarjumpTilesetHelper.Unload();
             ReverseKillbox.Unload();
+            ResizableDashSwitch.Load();
             //ChangeMaxCassetteTrigger.Unload();
+        }
+
+        public override void LoadContent(bool firstLoad)
+        {
+            base.LoadContent(firstLoad);
+
+            _CustomEntitySpriteBank = new SpriteBank(GFX.Game, "Graphics/SSMHelper/CustomEntitySprites.xml");
+
+            ResizableDashSwitch.LoadParticles();
         }
     }
 }
