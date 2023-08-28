a51efa5ff0aa153ba333e5fc41c1f60abdef1f24
SSM240 <supersupermario077@gmail.com>
2023-08-27 21:08:52 -0400

    make some code shared, move commands to own file

---
+++ lua/autorun/watereditor_autorun.lua
@@ -0,0 +1,24 @@
+function WaterFog_Initialize()
+    -- populate allWaterMaterials with every water material in the map and their properties
+    if not allWaterMaterials then
+        allWaterMaterials = {}  -- intentionally global
+        for _, ent in ipairs(ents:GetAll()) do
+            for _, brush in ipairs(ent:GetBrushSurfaces() or {}) do
+                if brush:IsWater() then
+                    local material = brush:GetMaterial()
+                    local materialName = material:GetName()
+                    if not allWaterMaterials[materialName] then
+                        allWaterMaterials[materialName] = {
+                            aboveWater = tobool(material:GetInt("$abovewater")),
+                            orig = {
+                                fogColor = material:GetVector("$fogcolor"),
+                                fogStart = material:GetFloat("$fogstart"),
+                                fogEnd = material:GetFloat("$fogend"),
+                            }
+                        }
+                    end
+                end
+            end
+        end
+    end
+end
