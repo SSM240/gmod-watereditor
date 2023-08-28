bed41e1bebd2d8e93e7f33f7fd3a93d8652d5268
SSM240 <supersupermario077@gmail.com>
2023-08-27 22:31:15 -0400

    comment change

--- lua/entities/edit_water.lua
+++ lua/entities/edit_water.lua
@@ -18,7 +18,7 @@ function ENT:Initialize()
 end
 
 function ENT:SetupDataTables()
-    -- this feels like it should go in Initialize but that doesn't work as well for some reason
+    -- this feels like it should go in Initialize but this works better for some reason
     WaterEdit_Initialize()
 
     local function GetComboOptions()
