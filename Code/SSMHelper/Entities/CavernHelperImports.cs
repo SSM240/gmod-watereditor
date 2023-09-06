8eee314c5505168377614c73ea1bb1bcc42b90bc
SSM240 <supersupermario077@gmail.com>
2023-09-06 05:35:16 -0400

    add Crystal Bomb Badeline Boss
    some of the CavernHelperImports stuff requires a pending change to Cavern Helper, but should otherwise work fine

---
+++ Code/SSMHelper/Entities/CavernHelperImports.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Xna.Framework;
+using Monocle;
+using MonoMod.ModInterop;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Celeste.Mod.SSMHelper.Entities
+{
+    [ModImportName("CavernHelper")]
+    public static class CavernHelperImports
+    {
+        public static Func<Action<Vector2>, Collider, Component> GetCrystalBombExplosionCollider;
+        public static Func<Collider, Component> GetCrystalBombExploderCollider;
+    }
+}
